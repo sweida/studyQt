066446d1e906bdde38150aef64391093ae174e23
sweida <weidaxyy@163.com>
2019-01-21 21:43:43 +0800

    qt

---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //    开始页面，车辆管理
+//    on_actionCar_triggered();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionCar_triggered()
+{
+    //    切换到车辆管理页面
+    ui->stackedWidget->setCurrentWidget(ui->car);
+    ui->label->setText("车辆管理");
+}
+
+void MainWindow::on_actionCalc_triggered()
+{
+    //    切换到销售统计
+    ui->stackedWidget->setCurrentWidget(ui->calc);
+    ui->label->setText("销售统计");
+}
+
+
+
+
---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+//3行解决中文乱码
+#ifdef WIN32
+#pragma execution_character_set("utf-8")
+#endif
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionCar_triggered();
+
+    void on_actionCalc_triggered();
+
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
